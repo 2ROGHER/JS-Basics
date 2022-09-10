c4c50870d1409ebad8631099d2251609ff49327f
ROGER MESTANZA <rmestanza.217@gmail.com>
2022-09-10 09:36:34 -0500

    commit lesson2-JS to BootCamp

---
+++ lesson2-JS.js
@@ -0,0 +1,13 @@
+// TASK 02 SOLVED
+const worker =[
+    "Roger",
+    23,
+    True,
+    "08-02-1999",
+    favoriteBook={
+        tittle: "Analisis Matematico I",
+        autor: "Eduardo Espinoza Ramos",
+        date: '10-05-2012',
+        url: 'https://www.academia.edu/43260495/Analisis_Matematico_2_Eduardo_Espinoza_Ramos?sm=b'
+    }
+]
